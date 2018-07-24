ec1c8513654ccc8d57b623de6407d87aad5f1d92
Mark Roach <mrroach@mrroach-macbookpro.roam.corp.google.com>
2018-07-23 23:13:54 -0700

    Update with all changes

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 
 class MainHandler(webapp2.RequestHandler):
+    """This shows our form."""
+
     def get(self):
         main_template = JINJA_ENVIRONMENT.get_template('templates/main.html')
-        self.response.write(main_template.render(template_vars))
+        self.response.write(main_template.render())
 
-    def post(self):
+
+class ResultsHandler(webapp2.RequestHandler):
+    """This shows the results of submitting the form."""
+
+    def get(self):
         template_vars = {
             'noun1':self.request.get("noun1"),
             'activity':self.request.get("activity"),
@@ -22,10 +28,11 @@ class MainHandler(webapp2.RequestHandler):
             'show':self.request.get("show"),
             'fun':self.request.get("fun"),
         } 
-        main_template = JINJA_ENVIRONMENT.get_template('templates/main.html')
-        self.response.write(main_template.render(template_vars))
+        results_template = JINJA_ENVIRONMENT.get_template('templates/results.html')
+        self.response.write(results_template.render(template_vars))
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
+    ('/results', ResultsHandler),
 ], debug=True)
